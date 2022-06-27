f232cb8fcdb541caac827c80f70a35de2634260c
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-27 13:17:42 +0530

    Finish the app :)

--- src/cadence/scripts/getTotalSupply_script.js
+++ src/cadence/scripts/getTotalSupply_script.js
@@ -1,11 +1,11 @@
 export const getTotalSupply =
-// REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
-`
-import BottomShot from 0x7b6adb682517f137;
+  // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
+  `
+import CarMoji from 0xe5de429200719cee;
 
 pub fun main(): UInt64 {
 
-    return BottomShot.totalSupply;
+    return CarMoji.totalSupply;
 
 }
-`
+`;
--- src/cadence/transactions/mintNFT_tx.js
+++ src/cadence/transactions/mintNFT_tx.js
@@ -1,9 +1,10 @@
-export const mintNFT = 
-`
+export const mintNFT = `
 // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
-import BottomShot from 0x7b6adb682517f137 
-// This remains the same 
+import CarMoji from 0xe5de429200719cee
+
+// Do not change these
 import NonFungibleToken from 0x631e88ae7f1d7c20
+import MetadataViews from 0x631e88ae7f1d7c20
 
 transaction(
   recipient: Address,
@@ -12,20 +13,20 @@ transaction(
   thumbnail: String,
 ) {
   prepare(signer: AuthAccount) {
-    if signer.borrow<&BottomShot.Collection>(from: BottomShot.CollectionStoragePath) != nil {
+    if signer.borrow<&CarMoji.Collection>(from: CarMoji.CollectionStoragePath) != nil {
       return
     }
 
     // Create a new empty collection
-    let collection <- BottomShot.createEmptyCollection()
+    let collection <- CarMoji.createEmptyCollection()
 
     // save it to the account
-    signer.save(<-collection, to: BottomShot.CollectionStoragePath)
+    signer.save(<-collection, to: CarMoji.CollectionStoragePath)
 
     // create a public capability for the collection
-    signer.link<&{NonFungibleToken.CollectionPublic}>(
-      BottomShot.CollectionPublicPath,
-      target: BottomShot.CollectionStoragePath
+    signer.link<&{NonFungibleToken.CollectionPublic, MetadataViews.ResolverCollection}>(
+      CarMoji.CollectionPublicPath,
+      target: CarMoji.CollectionStoragePath
     )
   }
 
@@ -33,19 +34,19 @@ transaction(
   execute {
     // Borrow the recipient's public NFT collection reference
     let receiver = getAccount(recipient)
-      .getCapability(BottomShot.CollectionPublicPath)
+      .getCapability(CarMoji.CollectionPublicPath)
       .borrow<&{NonFungibleToken.CollectionPublic}>()
       ?? panic("Could not get receiver reference to the NFT Collection")
 
     // Mint the NFT and deposit it to the recipient's collection
-    BottomShot.mintNFT(
+    CarMoji.mintNFT(
       recipient: receiver,
       name: name,
       description: description,
       thumbnail: thumbnail,
     )
-    
+
     log("Minted an NFT and stored it into the collection")
-  } 
+  }
 }
-`
+`;
